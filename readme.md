58a446dd24b279741015ce903ac47b9b8c037100
Ashish Jha <ashishjha14411@gmail.com>
2022-09-20 19:18:33 +0530

    pushing all files to git repo

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Description
+This project is part of JS assignment Challenge. It uses the jokeapi API, its free to use and does not require authentication token. It shows you one joke at a time. Enjoy the Jokes 
+
+## Time Taken
+
+1 hour approximately
+
+[Live Demo]()
+
+### SnapShot of the program
---
+++ script.js
@@ -0,0 +1,17 @@
+let jokeArea = document.querySelector("#joke");
+const getJokeBtn = document.querySelector("#jokeBtn");
+
+getJokeBtn.addEventListener("click", async () => {
+  let response = await fetch("https://v2.jokeapi.dev/joke/Dark");
+  let data = await response.json();
+  if (data.setup == "") {
+    jokeArea.innerHTML = data.delivery;
+  } else {
+    jokeArea.innerHTML = data.setup;
+    jokeArea.innerHTML += "\n" + data.delivery;
+  }
+  if (data.setup === undefined || data.delivery === undefined) {
+    jokeArea.innerHTML =
+      "There has been an error due to the lack of support for the api development, Kindly Contact the contributors at jokeapi.dev to help";
+  }
+});
